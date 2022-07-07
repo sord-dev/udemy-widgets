4d6e9de096935c1d2d05b765bb92ee09689076dc
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-07-07 11:35:11 +0100

    scafolded translate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-import "./App.css";
 import Color from "./components/Color";
+import "./App.css";
 
 function App() {
   return (
@@ -10,9 +10,7 @@ function App() {
       className="ui fluid container"
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-
-    <Color />
-
+      <Translate />
     </div>
   );
 }
--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "./Dropdown";
 
+const options = [
+  {
+    label: 'Japanese', value: 'jp'
+  },
+  {
+    label: 'Finnish', value: 'fi'
+  },
+  {
+    label: 'English', value: 'en'
+  },
+] 
+
 function Translate() {
+  const [select, setSelect] = useState(options[2]);
+  const [phrase, setPhrase] = useState('')
+  
   return (
     <div className="ui container center aligned" style={{ margin: "10px" }}>
       <div className="ui input">
-        <input type="text" placeholder="Enter text..." />
+        <input type="text" placeholder="Enter text..." value={phrase} onChange={(e) => setPhrase(e.target.value)}/>
       </div>
 
-      <Dropdown />
+      <Dropdown options={options} select={select} setSelect={setSelect} />
+
+      <h1>{phrase}</h1>
+      <h3>{`This text will be displayed in ${select.label}.`}</h3>
     </div>
   );
 }
