d56923c21978b112a091a37a98bdc89d039b56b8
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-07-07 18:30:50 +0100

    edited styles for components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -32,7 +32,7 @@ function Search() {
   }, [debounce]);
 
   return (
-    <div className='ui container fluid'>
+    <div className='ui container fluid' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <div className="ui left icon input" style={{ marginBottom: "15px" }}>
         <i className="search icon"></i>
         <input
